f9441ddcb54604df072843cc17b44093e3b4ba9e
Noam Gallmann <ng@daedalean.ai>
2023-02-20 15:07:28 +0100

    Add gitgutter

--- .config/nvim/nvim/lua/ng/packer.lua
+++ .config/nvim/nvim/lua/ng/packer.lua
@@ -32,6 +32,7 @@ return require('packer').startup(function(use)
     --use('theprimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
+    use('airblade/vim-gitgutter')
     use("nathom/filetype.nvim")
     -- Fancier statusline
     use {
