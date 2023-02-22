7be43a6c69c135a3f6314e674c3274db73fc3eb8
Noam Gallmann <ng@daedalean.ai>
2023-02-22 09:36:37 +0100

    Source the config only once

--- sourceme/sourceme.sh
+++ sourceme/sourceme.sh
@@ -1,26 +1,37 @@
 #!/usr/bin/env bash
 
-# Set repo root variable
 ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-ROOT="$ROOT/.."
+echo "Sourcing workspace config from $ROOT"
+
+if [[ -z "$WORKSPACE_SET" ]]; then
+ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    # Set repo root variable
+    ROOT="$ROOT/.."
 
 
-# Enable dbt command competion
-. $ROOT/sourceme/dbt-completion.sh
 
-# Export binaries to path
-echo "Exporting binaries to PATH; prepending it to existing $PATH variable, giving them priority over system binaries."
-export PATH="$(find "$ROOT" -type d -name bin -printf '%p:')"$PATH
+    # Enable dbt command competion
+    . $ROOT/sourceme/dbt-completion.sh
 
-# Export configuration paths
-export CONFIGROOT=$ROOT/.config
-export SCREENRC=$CONFIGROOT/screen/screenrc
-export NVIMCONFIG=$CONFIGROOT/nvim
+    # Export binaries to path
+    export PATH="$(find "$ROOT" -type d -name bin -printf '%p:')"$PATH
 
-# Replace oldvim
-echo "Setting aliases"
-alias nvim="XDG_DATA_HOME=$NVIMCONFIG/share XDG_CONFIG_HOME=$NVIMCONFIG nvim"
-alias vim="XDG_DATA_HOME=$NVIMCONFIG/share XDG_CONFIG_HOME=$NVIMCONFIG nvim"
-alias vi="XDG_DATA_HOME=$NVIMCONFIG/share XDG_CONFIG_HOME=$NVIMCONFIG nvim"
-alias vimdiff="XDG_DATA_HOME=$NVIMCONFIG/share XDG_CONFIG_HOME=$NVIMCONFIG nvim -d"
-export EDITOR=nvim
+    # Export configuration paths
+    export CONFIGROOT=$ROOT/.config
+    export SCREENRC=$CONFIGROOT/screen/screenrc
+    export NVIMCONFIG=$CONFIGROOT/nvim
+
+    # Replace oldvim
+    echo "Setting aliases"
+    alias nvim="XDG_DATA_HOME=$NVIMCONFIG/share XDG_CONFIG_HOME=$NVIMCONFIG nvim"
+    alias vim="XDG_DATA_HOME=$NVIMCONFIG/share XDG_CONFIG_HOME=$NVIMCONFIG nvim"
+    alias vi="XDG_DATA_HOME=$NVIMCONFIG/share XDG_CONFIG_HOME=$NVIMCONFIG nvim"
+    alias vimdiff="XDG_DATA_HOME=$NVIMCONFIG/share XDG_CONFIG_HOME=$NVIMCONFIG nvim -d"
+    export EDITOR=nvim
+
+    echo "You are all set."
+else
+    echo "Already set"
+fi
+
+export WORKSPACE_SET=1
