76095e685cf62c9d5bb66184c515ea55cb4b8665
Noam Gallmann <ng@daedalean.ai>
2023-06-22 08:20:42 +0200

    move resizing keybinding away from go to previeous buffer, disable clashing seldom used keybinding

--- .config/nvim/nvim/after/plugin/telescope.lua
+++ .config/nvim/nvim/after/plugin/telescope.lua
@@ -20,5 +20,6 @@ vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current 
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
 vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
 vim.keymap.set('n', '<leader>fo', builtin.find_files, { desc = '[F]ind [F]iles' })
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+-- clashes whith split resizing
+-- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>bb', builtin.buffers, {})
