86c7a0b6bba4b78158707b9ef4fc47cc60c35983
Noam Gallmann <ng@daedalean.ai>
2023-12-14 10:24:02 +0100

    Update ALE command for new dbt-rules

--- config/nvim/nvim/after/plugin/ale.lua
+++ config/nvim/nvim/after/plugin/ale.lua
@@ -1,4 +1,4 @@
-vim.g.ale_verilog_vlog_options ='-work BUILD/OUTPUT/*_lib -quiet -lint'
+vim.g.ale_verilog_vlog_options ='-work BUILD/OUTPUT/questa_lib/work -quiet -lint'
 
 -- let g:ale_linters = { 'systemverilog': ['verible', 'verilator', 'vlog']}
 -- let g:ale_linters = { 'verilog': ['verible', 'verilator']}
