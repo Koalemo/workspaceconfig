9e5be7fc77a2f1b1f013e59aa844af1572a1a1b8
Noam Gallmann <ng@daedalean.ai>
2025-01-07 13:50:21 +0100

    Fix lualine buffer status bar

--- config/nvim/nvim/after/plugin/lualine.lua
+++ config/nvim/nvim/after/plugin/lualine.lua
@@ -1,8 +1,16 @@
 require('lualine').setup {
   options = {
-    icons_enabled = true,
+    icons_enabled = false,
     theme = 'codedark',
     component_separators = '|',
     section_separators = '',
+  },
+   sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'filename'},
+    lualine_c = {'encoding',  'filetype'},
+    lualine_x = {'branch', 'diff', 'diagnostics'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'},
   },
 }
