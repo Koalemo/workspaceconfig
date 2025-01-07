8aa0227892af3f1e420a01340794eb6b260a84d5
Noam Gallmann <ng@daedalean.ai>
2025-01-07 13:53:39 +0100

    Add filepath to lualine status bar

--- config/nvim/nvim/after/plugin/lualine.lua
+++ config/nvim/nvim/after/plugin/lualine.lua
@@ -7,7 +7,7 @@ require('lualine').setup {
   },
    sections = {
     lualine_a = {'mode'},
-    lualine_b = {'filename'},
+    lualine_b = {{'filename', path=1}},
     lualine_c = {'encoding',  'filetype'},
     lualine_x = {'branch', 'diff', 'diagnostics'},
     lualine_y = {'progress'},
