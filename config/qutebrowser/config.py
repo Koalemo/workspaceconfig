e0c6b7592150abe2dfbc03ceb6d0f99bdc0faa7c
Koalemo <gnoam@live.com>
2025-11-18 17:53:55 +0100

    Experimental: Qutebrowser keybindings

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,1 +1,20 @@
-c.tabs.position=left
+
+# Keybindings
+# config.bind("t", "cmd-set-text -s :open -t")
+# config.bind("O", "cmd-set-text -s :open -w")
+# config.bind("P", "cmd-set-text -s :open -p")
+# config.bind("W", "tab-clone -w")
+
+config.bind('<Ctrl-n>', 'completion-item-focus next', mode='command')
+config.bind('<Ctrl-p>', 'completion-item-focus prev', mode='command')
+
+config.bind("a", "mode-enter insert")
+
+config.bind("K", "tab-next")
+config.bind("J", "tab-prev")
+config.bind("H", "back")
+config.bind("L", "forward")
+config.bind("Q", "tab-close")
+
+#config.bind("<Ctrl-=>", "zoom-in")
+#config.bind("<Ctrl-->", "zoom-out")
