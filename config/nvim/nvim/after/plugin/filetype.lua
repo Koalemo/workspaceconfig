c5df18ee64717b6f5fe9648454cda2e65f38e1a0
ng <noam.gallmann@gmail.com>
2025-12-17 18:42:23 +0100

    Remove deprecated init file for filetype plugin

--- config/nvim/nvim/after/plugin/filetype.lua
+++
@@ -1,9 +0,0 @@
--- In init.lua or filetype.nvim's config file
-require("filetype").setup({
-    overrides = {
-        extensions = {
-            -- Set the filetype of *.pn files to potion
-            xdc = "tcl",
-        },
-    },
-})
